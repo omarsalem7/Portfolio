5a55a47dd152b205cbb43e678e303ab61f04568d
Omar <omarsalem0721@gmail.com>
2021-12-13 17:44:28 +0200

    fixing linters error

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 // close and open btns selectors
-const openIcon = document.querySelector(".navbar-humberger");
-const closeIcon = document.querySelector(".closeIcon");
+const openIcon = document.querySelector('.navbar-humberger');
+const closeIcon = document.querySelector('.closeIcon');
 
-const navbar = document.querySelector(".navbar");
-const header = document.querySelector(".header");
-const navbarOverlay = document.getElementById("navbar-overlay");
-
-openIcon.addEventListener("click", function () {
+const navbar = document.querySelector('.navbar');
+const header = document.querySelector('.header');
+const navbarOverlay = document.getElementById('navbar-overlay');
+function openLayoutFunction() {
   // creat layout
-  navbarOverlay.style.width = "100%";
+  navbarOverlay.style.width = '100%';
   // add blur in background
-  navbar.classList.add("blur");
-  header.classList.add("blur");
-});
+  navbar.classList.add('blur');
+  header.classList.add('blur');
+}
+openIcon.addEventListener('click', openLayoutFunction);
 
-closeIcon.addEventListener("click", function () {
+function closeLayoutFunction() {
   //remove the overlay
-  navbarOverlay.style.width = "0%";
+  navbarOverlay.style.width = '0%';
   // remove blur from background
-  navbar.classList.remove("blur");
-  header.classList.remove("blur");
-});
+  navbar.classList.remove('blur');
+  header.classList.remove('blur');
+}
 
-const overlay_content = document.querySelector(".overlay-content");
-overlay_content.addEventListener("click", function () {
-  navbarOverlay.style.width = "0%";
-  navbar.classList.remove("blur");
-  header.classList.remove("blur");
-});
+closeIcon.addEventListener('click', closeLayoutFunction);
+
+const overlay_content = document.querySelector('.overlay-content');
+
+function clickOnMenuLinkfunction() {
+  navbarOverlay.style.width = '0%';
+  navbar.classList.remove('blur');
+  header.classList.remove('blur');
+}
+overlay_content.addEventListener('click', clickOnMenuLinkfunction);
