ce090ce871af143ff335f127624f7e85b3043d8c
Omar <omarsalem0721@gmail.com>
2021-12-15 17:47:27 +0200

    Add linter locally

--- validate.js
+++ validate.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('.contact-form');
 const emailMsg = document.querySelector('.my-email-label');
-
 function testEmail(email) {
   const emailPattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
   return emailPattern.test(email);
