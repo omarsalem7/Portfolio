beadba246e14ff369159cf416d02f5f4e78a250e
Omar <omarsalem0721@gmail.com>
2021-12-16 15:07:32 +0200

    Fixing linters

--- scripts/preserveData.js
+++ scripts/preserveData.js
@@ -2,25 +2,24 @@ const myForm = document.querySelector('.contact-form');
 const inputSeletors = document.querySelectorAll('.form-control');
 const userData = {};
 
-//=============== get data from localstorgae as js object ==============
+// =============== get data from localstorgae as js object ==============
 function getLocalStorageData() {
   const jsonString = localStorage.getItem('userData');
   const retrievedObject = JSON.parse(jsonString);
-  console.log(retrievedObject);
   return retrievedObject;
 }
-//======================================================================
+// ======================================================================
 
-//======= Add event listner when an input change in form ===============
+// ======= Add event listner when an input change in form ===============
 myForm.addEventListener('change', () => {
   userData.name = inputSeletors[0].value;
   userData.email = inputSeletors[1].value;
   userData.message = inputSeletors[2].value;
   localStorage.setItem('userData', JSON.stringify(userData));
 });
-//======================================================================
+// ======================================================================
 
-//============ fill data from localstorgae to input value ==============
+// ============ fill data from localstorgae to input value ==============
 function fillFormWithData() {
   const savedData = getLocalStorageData();
   inputSeletors.forEach((input) => {
@@ -28,4 +27,4 @@ function fillFormWithData() {
   });
 }
 fillFormWithData();
-//======================================================================
+// ======================================================================
